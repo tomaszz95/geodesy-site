788a477bb72a6c309e6a86b753810ec500c4c371
Tomasz Ż <tzuber95@gmail.com>
2023-03-06 16:39:11 +0100

    ended navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const burgerBtn = document.querySelector('.nav-mobile__burger')
+const navigation = document.querySelector('.nav-mobile__links')
+
+const navigationHandler = e => {
+	if (e.target.classList.value == 'nav-mobile__burger' || 'nav-mobile__burger--bar') {
+		burgerBtn.classList.toggle('active')
+		navigation.classList.toggle('active')
+	}
+}
+
+const closeNavigation = e => {
+	if (e.target.classList.value == 'nav-mobile__link') {
+		burgerBtn.classList.remove('active')
+		navigation.classList.remove('active')
+	}
+}
+
+burgerBtn.addEventListener('click', navigationHandler)
+navigation.addEventListener('click', closeNavigation)
