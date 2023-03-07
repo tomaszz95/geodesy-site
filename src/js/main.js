1c412f31bf942ff08379ae4d81b1a0953cd5ef57
Tomasz Ż <tzuber95@gmail.com>
2023-03-07 17:01:38 +0100

    end news and info sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 const burgerBtn = document.querySelector('.nav-mobile__burger')
 const navigation = document.querySelector('.nav-mobile__links')
+const newsBoxes = document.querySelectorAll('.news__box')
+let boxesNumber = 0
 
+// NAVIGATION
 const navigationHandler = e => {
 	if (e.target.classList.value == 'nav-mobile__burger' || 'nav-mobile__burger--bar') {
 		burgerBtn.classList.toggle('active')
@@ -18,8 +21,34 @@ const closeNavigation = e => {
 	if (e.target.classList.value == 'nav-mobile__link') {
 		burgerBtn.classList.remove('active')
 		navigation.classList.remove('active')
+		document.body.style.overflowY = 'auto'
 	}
 }
 
+// NEWS BOXES
+const newsBoxesAnimation = () => {
+	newsBoxes.forEach(box => {
+		if (
+			box.offsetTop - scrollY < 850 &&
+			!box.classList.contains('animationRight') &&
+			!box.classList.contains('animationLeft') &&
+			boxesNumber % 2 === 0
+		) {
+			box.classList.add('animationRight')
+		} else if (
+			box.offsetTop - scrollY < 850 &&
+			!box.classList.contains('animationLeft') &&
+			!box.classList.contains('animationRight') &&
+			boxesNumber % 2 === 1
+		) {
+			box.classList.add('animationLeft')
+		} else {
+			return
+		}
+		boxesNumber++
+	})
+}
+
+window.addEventListener('scroll', newsBoxesAnimation)
 burgerBtn.addEventListener('click', navigationHandler)
 navigation.addEventListener('click', closeNavigation)
