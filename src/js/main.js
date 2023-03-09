b864286a1636b2e23a8fa2a9b168961ef3b57eeb
Tomasz Ż <tzuber95@gmail.com>
2023-03-09 14:11:14 +0100

    end references in aboutus page

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 const burgerBtn = document.querySelector('.nav-mobile__burger')
 const navigation = document.querySelector('.nav-mobile__links')
 const newsBoxes = document.querySelectorAll('.news__box')
+const thumbnails = document.querySelectorAll('.aboutus__details-gallery-img')
+const popup = document.querySelector('.aboutus__details-popup')
+const popupClose = document.querySelector('.aboutus__details-popup--close')
+const popupImg = document.querySelector('.aboutus__details-popup--img')
+const arrowLeft = document.querySelector('.aboutus__details-popup--arrow-prev')
+const arrowRight = document.querySelector('.aboutus__details-popup--arrow-next')
 let footerYear = document.querySelector('.footer__year')
 let boxesNumber = 0
+let currentImgIndex
 
 // NAVIGATION
 const navigationHandler = e => {
@@ -50,6 +57,47 @@ const newsBoxesAnimation = () => {
 	})
 }
 
+// POPUP MANAGING
+const showNextImg = () => {
+	if (currentImgIndex === thumbnails.length - 1) {
+		currentImgIndex = 0
+	} else {
+		currentImgIndex++
+	}
+	popupImg.src = thumbnails[currentImgIndex].src
+}
+
+const showPrevImg = () => {
+	if (currentImgIndex === 0) {
+		currentImgIndex = thumbnails.length - 1
+	} else {
+		currentImgIndex--
+	}
+	popupImg.src = thumbnails[currentImgIndex].src
+}
+
+const closePopup = () => {
+	popup.classList.add('hidden')
+}
+
+thumbnails.forEach((thumbnail, index) => {
+	const showPopup = e => {
+		popup.classList.remove('hidden')
+		popupImg.src = e.target.src
+		currentImgIndex = index
+		thumbnails.forEach(element => {
+			element.setAttribute('tabindex', -1)
+		})
+	}
+	thumbnail.addEventListener('click', showPopup)
+
+	thumbnail.addEventListener('keydown', e => {
+		if (e.code === 'Enter' || e.key === 13) {
+			showPopup(e)
+		}
+	})
+})
+
 // FOOTER YEAR
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -60,3 +108,22 @@ handleCurrentYear()
 window.addEventListener('scroll', newsBoxesAnimation)
 burgerBtn.addEventListener('click', navigationHandler)
 navigation.addEventListener('click', closeNavigation)
+popupClose.addEventListener('click', closePopup)
+arrowRight.addEventListener('click', showNextImg)
+arrowLeft.addEventListener('click', showPrevImg)
+document.addEventListener('keydown', e => {
+	if (!popup.classList.contains('hidden')) {
+		if (e.code === 'ArrowRight' || e.key === 39) {
+			showNextImg()
+		} else if (e.code === 'ArrowLeft' || e.key === 37) {
+			showPrevImg()
+		} else if (e.code === 'Escape' || e.key === 27) {
+			closePopup()
+		}
+	}
+})
+popup.addEventListener('click', e => {
+	if (e.target === popup) {
+		closePopup()
+	}
+})
